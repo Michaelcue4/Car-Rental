24ea6ec5dff533b736c016ae18fc8fed4a40c546
Michaelcue4 <michaelcue@me.com>
2019-05-15 11:00:05 -0400

    heroku correction

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Hello World </h1>
+          <h1>Hello World
+            this is the main test page  </h1>
       </header>
     </div>
   );
