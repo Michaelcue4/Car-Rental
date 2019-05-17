fe4966319827fda7b81efd61901051caecb26c41
Michaelcue4 <michaelcue@me.com>
2019-05-17 10:54:51 -0400

    testing seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,11 +1,17 @@
-const customer = require('../models/customer')
-const Reservation = require('../models/reservation')
+const Customer = require('../models/customer')
+// const Reservation = require('../models/reservation')
 
 
 const makeCustomerSampleData = async () => {
-    await customer.deleteAllCustomers();
-    await customer.createCustomer({name: 'Damien'})
-    await customer.createCustomer({name: 'Michael'})
+    // await customer.deleteAllCustomers();
+    // await customer.createCustomer({name: 'Damien'})
+    // await customer.createCustomer({name: 'Michael'})
+    await Customer.deleteMany()
+    const luke = new Customer({name: 'Luke'})
+    await luke.save()
+    const darth = new Customer({name: 'Darth Vader'})
+    await darth.save()
+
 }
 
-await makeCustomerSampleData();
+makeCustomerSampleData();
