005fb670a9297d1aeb6130adf318383686ff37ce
Michaelcue4 <michaelcue@me.com>
2019-05-17 15:18:05 -0400

    updating routes

--- controller/customerController.js
+++ controller/customerController.js
@@ -6,6 +6,36 @@ const customerController = {
     Customer.find().then(customerList=>{
         //console.log(libraryList);
         res.render('customer/index',{customerList})
-    })
+    }),
+    new:(req,res)=>{
+        res.render('customer/new')
+    },
+    create:(req,res)=>{
+        Customer.create(req.body).then(() => {
+            res.redirect("customer/index");
+          })
+    },
+    show:(req,res)=>{
+        const customerId = req.params.id 
+        Library.findById(customerId).then((SingleCustomer)=>{
+            res.render('customer/show',{SingleCustomer});
+        })
+    },edit:(req,res)=>{
+        Library.findById(req.params.id).then(Singlelibrary=>{
+            res.render('customer/edit', {Singlelibrary})
+        })
+    },
+    update:(req,res)=>{
+        Customer.updateOne({_id : req.params.id}, req.body).then(() => {
+            res.redirect("/" + req.params.id);
+          })
+    },
+    delete:(req,res)=>{
+        Library.deleteOne({"_id":req.params.id}).then(()=>{
+            res.redirect("/")
+        })
+    }
+
+
 }
 module.exports = customerController;
--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,5 @@ const router = express.Router()
 const customerController = require('../controller/customerController')
 // Routes
 router.get('/',customerController.index);
+router.post('/',customerController.create);
+router.get('/new',customerController.new);
